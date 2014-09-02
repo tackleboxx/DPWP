e0897be836fce45710efd6854e419004fae4e08b
tackleboxx <tackleboxxdesigns@gmail.com>
2014-09-01 20:40:26 -0400

    setting new files up

---
+++ intro to python/main.py
@@ -0,0 +1,11 @@
+print "hello world!"
+#one line comment
+'''
+
+doc strings
+
+'''
+
+first_name = "kermit"
+last_name = "De Frog"
+print first_name
---
+++ simple-login/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+'''
+name
+date
+class
+assignment
+'''
+import webapp2 #use the webapp2 library
+
+class MainHandler(webapp2.RequestHandler): # declaring a class
+    def get(self): #function that starts
+        self.response.write('Hello world!')
+        #code go here
+
+    def additional_functions(self):
+        pass
+        #code go here
+
+
+#never touch
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
