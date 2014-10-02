43e007550e06b043cf9328789d8a6b35a686cb02
tackleboxx <robertdunn@roberts-mbp.gateway.pace.com>
2014-10-01 21:59:44 -0400

    started assignment added first question

--- madlib/main.py
+++ madlib/main.py
@@ -9,3 +9,32 @@ Name: Danny Dunn
 
 
 print "This is my MadLib Assignment by Danny Dunn"
+
+# First Question
+
+first_place = raw_input(" Enter A Place")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# This is the section for the Story
+
+print " Going Out "
+
+print "Today I was thinking about taking my dog out for a walk.  Should I take my dog to the ", first_place ,"?"
