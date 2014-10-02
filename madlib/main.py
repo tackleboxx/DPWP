c65b8bb85322f7de91217d011263d77633aace88
tackleboxx <robertdunn@roberts-mbp.gateway.pace.com>
2014-10-01 22:22:00 -0400

    finished adding strings and numbers for madlib

--- madlib/main.py
+++ madlib/main.py
@@ -12,14 +12,27 @@ print "This is my MadLib Assignment by Danny Dunn"
 
 # First Question
 
-first_place = raw_input(" Enter A Place")
+first_place = raw_input(" Enter A Place ")
 
+# Second Question
 
+second_place = raw_input(" Enter Another Place ")
 
+# Third Question
 
+third_place = raw_input(" Enter A Place You Never Been ")
 
+# First Number
 
+first_number = raw_input(" Enter A Number ")
 
+# Second Number
+
+second_number = raw_input(" Enter Another Number ")
+
+# Third Number
+
+third_number = raw_input(" Enter A Different Number ")
 
 
 
@@ -38,3 +51,4 @@ first_place = raw_input(" Enter A Place")
 print " Going Out "
 
 print "Today I was thinking about taking my dog out for a walk.  Should I take my dog to the ", first_place ,"?"
+print "Do you think he would have a better time at the ", second_place, "?"
