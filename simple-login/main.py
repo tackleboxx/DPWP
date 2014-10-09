035afa1ef87876092c865911a01f535ef7d4b946
tackleboxx <robertdunn@roberts-mbp.gateway.pace.com>
2014-10-09 12:10:08 -0400

    started form and added html to page, and added form elements

--- simple-login/main.py
+++ simple-login/main.py
@@ -8,7 +8,19 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): # declaring a class
     def get(self): #function that starts
-        self.response.write('Hello world!')
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>
+        <form method="GET">
+        <label>Name:</label><input type="text" />
+        <label>Email:</label><input type="text" />
+        </form>
+    </body>
+</html>'''
+        self.response.write(page)
         #code go here
 
     def additional_functions(self):
