e74753bfbaab73bc570e25df6b6770d86242fd50
tackleboxx <tackleboxxdesigns@gmail.com>
2014-10-09 13:39:05 -0400

    added css files

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 '''
 name
 date
@@ -25,7 +8,33 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): # declaring a class
     def get(self): #function that starts
-        self.response.write('Hello world!')
+
+        page_head = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+        <link href="{self.css}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>'''
+
+
+        page_body = '''<form method="GET" action="">
+        <label>Name:</label><input type="text" name="user" />
+        <label>Email:</label><input type="text" name="email" />
+        <input type="submit" value="submit" />'''
+
+        page_close = '''
+        </form>
+    </body>
+</html>'''
+
+        if self.request.GET:
+            #stores info we get from form
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            self.response.write(page_head + user + ' ' + page_body + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close) #printing page out
         #code go here
 
     def additional_functions(self):
